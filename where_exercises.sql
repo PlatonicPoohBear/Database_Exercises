a0b01ab93790de801ed91bfe57c8aa26f68a1132
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-15 14:38:35 -0500

    Update to use 'and' and 'or'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,19 +1,36 @@
 select *
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya');
+where gender = 'M'
+	and (
+		first_name = 'Irena'
+		or first_name = 'Vidya'
+		or first_name = 'Maya'
+		);
 
 select *
 from employees
-where last_name like 'E%';
+where last_name like 'E%'
+	or last_name like '%e';
+	
 
 select *
 from employees
-where hire_date between '1990-01-01' and '1999-12-31';
+where last_name like 'E%'
+	and last_name like '%e';
+
+
+select *
+from employees
+where hire_date between '1990-01-01' and '1999-12-31'
+	and birth_date like '%12-25';
+
 
 select *
 from employees
 where birth_date like '%12-25';
 
+
 select *
 from employees
-where last_name like '%q%';
+where last_name like '%q%'
+ and last_name not like '%qu%';
