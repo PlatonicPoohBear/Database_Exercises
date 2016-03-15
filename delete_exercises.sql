22ea60ea22cd5ec98932289e80e889d06e96ae98
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-15 11:32:17 -0500

    Create delete_exercises for MySql database

---
+++ delete_exercises.sql
@@ -0,0 +1,8 @@
+use codeup_test_db;
+
+
+delete from albums where release_date > "1991-01-01";
+
+delete from albums where genre like "%disco%";
+
+delete from albums where artist = "Whitney Houston";
